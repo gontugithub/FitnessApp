bd3bf9f7825ba024cd5d8cb8dd1d87a1b43ec8ac
gontugithub <gs.alvarez8@gmail.com>
2024-10-10 18:24:50 +0200

    RegisterActivity done

--- app/src/main/java/com/example/fitnessapp/RegisterActivity.java
+++ app/src/main/java/com/example/fitnessapp/RegisterActivity.java
@@ -1,8 +1,6 @@
 package com.example.fitnessapp;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,28 +8,18 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class LoginActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_login);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.glTopBtIniciarSesion), (v, insets) -> {
+        setContentView(R.layout.activity_register);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
         getSupportActionBar().hide();
-
-
-
-
-    }
-
-    public void changePruebaActivity(View view){
-        startActivity(new Intent(LoginActivity.this, RegisterActivity.class ));
-
     }
 }

719b77b6e6aa07fc1ea7d11be3011dec1d46061e
gontugithub <gs.alvarez8@gmail.com>
2024-10-29 23:33:23 +0100

    login done with error control

--- app/src/main/java/com/example/fitnessapp/LoginActivity.java
+++ app/src/main/java/com/example/fitnessapp/LoginActivity.java
@@ -3,12 +3,26 @@ package com.example.fitnessapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.example.fitnessapp.models.User;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.Map;
 
 public class LoginActivity extends AppCompatActivity {
     public static String username; // STATIC SOLO TIENE UNA VARIABLE EN TODO
@@ -44,6 +58,71 @@ public class LoginActivity extends AppCompatActivity {
 
     public void changeRegisterActivty(View view){
         startActivity(new Intent(LoginActivity.this, RegisterActivity.class ));
+
+    }
+
+    private boolean isEmpty(EditText etText) {
+        return etText.getText().toString().trim().length() == 0;
+    }
+
+    public void checkEmptyInputsLogin(View view){
+
+
+
+        TextInputEditText email = findViewById(R.id.l_inpt_email);
+        TextInputEditText password = findViewById(R.id.l_inpt_password);
+
+
+        if (isEmpty(email) || isEmpty(password)){
+            Toast.makeText(LoginActivity.this, "CAMPOS VACIOS", Toast.LENGTH_SHORT).show();
+        } else {
+
+
+        FirebaseFirestore database = FirebaseFirestore.getInstance();
+
+        database.collection("users").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                boolean flag = false;
+                ArrayList<User> users = new ArrayList<>();
+                for (QueryDocumentSnapshot document : task.getResult()) {
+                    Map<String, Object> documentObj = document.getData();
+                    users.add(new User(
+                            documentObj.get("name").toString(),
+                            documentObj.get("email").toString(),
+                            documentObj.get("password").toString()));
+                }
+
+                for (User user : users){
+
+                    if (user.getPassword().equals(password.getText().toString()) && user.getEmail().equals(email.getText().toString())){
+                        flag = true;
+                        break;
+                    }
+                }
+                if (flag){
+
+                    login(view,email.getText().toString(),password.getText().toString());
+
+                } else {
+
+                    Toast.makeText(LoginActivity.this, "EMAIL O CONTRSEÑA NO REGISTRADO", Toast.LENGTH_LONG).show();
+                }
+
+            }
+
+
+        });
+
+
+
+
+    }}
+
+    public void login(View view, String email, String password){
+
+        // ACCEDE A LA BASE DE DATOS LOCAL CON LOS PARAMETROS QUE ME PASEN Y DA ACCESO A LA HOME
+        changeHomeActivity(view);
 
     }
 
--- app/src/main/java/com/example/fitnessapp/NewExerciseActivity.java
+++ app/src/main/java/com/example/fitnessapp/NewExerciseActivity.java
@@ -3,12 +3,26 @@ package com.example.fitnessapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Source;
+
+import java.util.ArrayList;
+import java.util.Map;
 
 public class NewExerciseActivity extends AppCompatActivity {
 
@@ -30,4 +44,62 @@ public class NewExerciseActivity extends AppCompatActivity {
         startActivity(new Intent(NewExerciseActivity.this, AllExercises.class ));
 
     }
+
+    private boolean isEmpty(EditText etText) {
+        return etText.getText().toString().trim().length() == 0;
+    }
+
+    public void checkIfExist(View view){
+
+        String userId = "a";
+
+        TextInputEditText exercisename = findViewById(R.id.ne_inpt_nombre);
+        TextInputEditText weight = findViewById(R.id.ne_inpt_peso);
+        TextInputEditText reps = findViewById(R.id.ne_inpt_repeticiones);
+
+        if (isEmpty(exercisename) || isEmpty(weight) || isEmpty(reps)){
+
+            Toast.makeText(NewExerciseActivity.this, "RELLENA TODOS LOS CAMPOS", Toast.LENGTH_SHORT).show();
+
+        } else {
+
+            FirebaseFirestore database = FirebaseFirestore.getInstance();
+            Source src = Source.SERVER;
+
+            database.collection("users").document(userId)
+                    .get(src).addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                            DocumentSnapshot result = task.getResult();
+                            Map<String, Object> data =  result.getData();
+                            ArrayList exercises = (ArrayList) data.get("exercises");
+
+                            for (Object exercise : exercises){
+
+
+                            }
+
+
+                        }
+                    });
+
+
+
+
+
+
+
+        }
+
+
+
+    }
+
+    public void addNewExercise(View view, String userid, String exercisename, Double weight, int reps){
+
+
+
+    }
+
+
 }
